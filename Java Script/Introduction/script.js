3bf544ad9c76348bd5d774c63a1fbe9680693ac4
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-02-08 00:46:37 +0530

    practice

---
+++ Java Script/Introduction/script.js
@@ -0,0 +1,12 @@
+
+    alert("Hello World");
+    var a = prompt("Enter Your name: ");
+    console.log("Your Name is "+a);
+
+    var isTrue = confirm("Are you Sure about you goal?");
+    if(isTrue){
+        console.log("I am damn sure about my goal");
+    }
+    else{
+        console.log("I am not sure about my goal")
+    }
