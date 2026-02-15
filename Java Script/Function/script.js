09d86723dcde88fc8cd26969c0deec854fdfe2f0
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-02-15 16:19:43 +0530

    practice

--- Java Script/Function/script.js
+++ Java Script/Function/script.js
@@ -9,8 +9,22 @@ function nice(name){
 nice("Dibesh")
 
 
-function sum(a,b){
-    console.log(a+b)
+function sum(a,b,c=4){
+    // console.log(a+b)
+    return a+b+c;
 
 }
-sum(3,5)
+result1=sum(3,5)
+result2=sum(46,55)
+result3=sum(35,56)
+console.log("Sum of two number:",result1)
+console.log("Sum of two number:",result2)
+console.log("Sum of two number:",result3)
+
+
+
+//arrow function
+const func1=(x,y)=>{
+    console.log("i am a arraow function",x+y)
+}
+func1(56,86)
