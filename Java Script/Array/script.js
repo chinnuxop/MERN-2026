a29927104b1abbffd9c03376216571088eb153c2
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-02-17 01:17:37 +0530

    practice

---
+++ Java Script/Array/script.js
@@ -0,0 +1,40 @@
+let arr = [1,2,3,4,5,6,7]
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(typeof arr)
+console.log(arr.toString())
+console.log(arr.join(" and "))
+let a =[2,3,4,5,6,8]
+
+console.log(a.pop())
+console.log(a)
+console.log(a.push("Dibesh"))
+console.log(a)
+console.log(a.shift())
+console.log(a)
+console.log(a.unshift("Ankita"))
+console.log(a)
+console.log(delete a[2])
+console.log(a)
+
+
+ let a1 = [1,2,3]
+ let a2 = [4,5,6]
+ let a3 = [9,8,7]
+console.log( a1.concat(a2,a3))
+console.log(a1.sort())
+
+ let num = [1,3,5,2,6]
+// console.log(num.splice(0,1))
+// console.log(num)
+console.log(num.splice(1, 2, 4, 6, 8))
+// Start at index 1
+
+// Remove 2 elements
+
+// Insert 4, 6, 8
+console.log(num)
+//16:25
+
+
