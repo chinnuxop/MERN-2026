dd52efa4a85d7eecbc4c71644237e36c8991bcd7
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-05-14 22:57:08 +0530

    random color

---
+++ Java Script/Color the Boxes/script.js
@@ -0,0 +1,15 @@
+console.log("Script.js initializing");
+let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
+// min+(max-min)*Math.random()
+function getRandomColor() {
+    let val1 = Math.ceil(0+Math.random()*255);
+    let val2 = Math.ceil(0+Math.random()*255);
+    let val3 = Math.ceil(0+Math.random()*255);
+    return `rgb(${val1}, ${val2}, ${val3})`;
+
+}
+
+Array.from(boxes).forEach(e => {
+    e.style.backgroundColor = getRandomColor();
+})
